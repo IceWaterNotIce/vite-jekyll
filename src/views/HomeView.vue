4b8c53f055c71d6798af03c17d3254fa442e7a3d
冰水不冰 <icewaternotice@gmail.com>
2024-03-10 22:30:21 +0800

    Update .gitignore and _config.yml

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import image_gallery from '../includes/image_gallery.html?raw'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <div v-html="image_gallery"></div>
+  </main>
+</template>
