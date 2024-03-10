9eeacb265fc2c655c91bbac3ad9d2adda67cfccd
冰水不冰 <icewaternotice@gmail.com>
2024-03-10 23:24:37 +0800

    Delete unused images and files, update configuration, and add rollup-plugin-delete

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import del from "rollup-plugin-delete";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+
   ],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/index.html',
+      
+      plugins: [
+        del({ targets: "dist/*", hook: "generateBundle" }),
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+    
